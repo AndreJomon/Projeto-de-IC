54118e3e8f0429608d7ed87494ba86cccd999563
Andre <jomonzito2010@gmail.com>
2019-01-16 10:05:22 -0200

    Novos videos de teste e telas gerais

---
+++ Assets/Scripts/VideoToPlayOnStart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Video;
+using UnityEngine;
+
+public class VideoToPlayOnStart : MonoBehaviour {
+
+    VideoManager videoManager;
+    public VideoClip video;
+
+    private void Start()
+    {
+        videoManager = VideoManager.instance;
+        videoManager.PlayVideo(video);
+    }
+}
