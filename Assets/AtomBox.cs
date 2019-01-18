694b44eced7e37aac0acdd884679c7dfd9a073aa
Andre <jomonzito2010@gmail.com>
2019-01-18 08:07:39 -0200

    Nova fase sendo montada

---
+++ Assets/AtomBox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtomBox : MonoBehaviour {
+
+    public GameObject atomo;
+    public GameObject newAtomo;
+
+    void OnMouseDown()
+    {
+        newAtomo = Instantiate(atomo, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity);
+    }
+
+    private void OnMouseDrag()
+    {
+        newAtomo.GetComponent<DragOn>().OnMouseDrag();
+    }
+
+    ///Fazer ontrigger no objeto instanciado
+    ///Triggar quando entra um booleano para não se autodestruir
+
+
+
+}
---
+++ Assets/Scripts/DragOn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+
+public class DragOn : MonoBehaviour {
+    /*
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+
+    }
+    */
+
+    float distance = 10;
+
+    public void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = objPosition;
+    }
+}
