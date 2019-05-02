4fc3d9807e2e9ac93be924d11f3cf99087fd2767
Andre <jomonzito2010@gmail.com>
2019-05-02 18:05:55 -0300

    Início da implementação da tela de seleção de minigames.

---
+++ Assets/QuintalSceneManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuintalSceneManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] screen;
+    private int currentScreen;
+
+    public void ResetScreenPosition(GameObject screen)
+    {
+        screen.transform.position = new Vector3(0, 0, 0);
+    }
+}
