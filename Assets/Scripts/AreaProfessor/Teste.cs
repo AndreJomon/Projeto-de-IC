456e0b65819926707fa6c44ef1fc0fef60c106d2
Rafael Nicoletti <40860359+RafaelGNicoletti@users.noreply.github.com>
2019-05-07 17:07:46 -0300

    Área do professor
    Verificação de senha

---
+++ Assets/Scripts/AreaProfessor/Teste.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teste : MonoBehaviour
+{
+    public UnityEngine.UI.Text text;
+    
+    void Start()
+    {
+        PlayerPrefs.SetString("Senha", "SenhaTeste01");
+    }
+
+    public void CheckPassword()
+    {
+        string psw = GameObject.Find("Senha").GetComponent<UnityEngine.UI.InputField>().text;
+        string sPsw = PlayerPrefs.GetString("Senha");
+        if (string.Compare(psw, sPsw) == 0)
+        {
+            text.text = "Senha correta!";
+        }
+        else
+        {
+            text.text = "HaHaHa, vc não disse a palavra mágica!!!";
+        }
+    }
+}
