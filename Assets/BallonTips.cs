81d8b1ea5af56dce03ec68ac12acff86d6327580
Andre <jomonzito2010@gmail.com>
2019-05-13 14:05:02 -0300

    Scripts mudados de lugar, arrumado bug do merge (de novo), implementando o BallonTips

---
+++ Assets/BallonTips.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallonTips : MonoBehaviour
+{
+    [SerializeField]
+    public static GameObject ballonTips;
+    public static GameObject queijo;
+    private static Vector2 lampPositionModifier = new Vector2(90,90); //Criado arbitrariamente a partir de testes
+
+    public static GameObject CreateBallonText(string text, Vector3 lampPosition)
+    {
+        GameObject tempBallonTips;
+        lampPosition.x += lampPositionModifier.x;
+        lampPosition.y += lampPositionModifier.y;
+        tempBallonTips = Instantiate(ballonTips, lampPosition, Quaternion.identity, GameObject.Find("Canvas").transform);
+        tempBallonTips.GetComponent<BallonTips>().PutInfo(text);
+        return tempBallonTips;
+    }
+
+    public void PutInfo(string text)
+    {
+        GetComponentInChildren<Text>().text = text;
+    }
+}
---
+++ Assets/TesteLamp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TesteLamp : MonoBehaviour
+{
+    Vector3 position;
+
+    public void Text()
+    {
+        BallonTips.CreateBallonText("Aiaiaiaii que delicia", GameObject.Find("Lampada").transform.position);
+    }
+}
