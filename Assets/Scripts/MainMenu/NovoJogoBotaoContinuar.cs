fcd86c8766c4406012e436724bc879cf58862c27
gustavo <gustavo.castilho@ufabc.edu.br>
2019-05-23 10:53:13 -0300

    sound manager and stuff

---
+++ Assets/Scripts/MainMenu/NovoJogoBotaoContinuar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NovoJogoBotaoContinuar : MonoBehaviour
+{
+    private Button continuarButton;
+    private Text nomeInputText;
+    private Text serieInputText;
+
+    private void Awake()
+    {
+        continuarButton = GameObject.Find("continuar").GetComponent<Button>();
+        nomeInputText = GameObject.Find("InputFieldNome").GetComponentInChildren<Text>();
+        serieInputText = GameObject.Find("InputFieldSerie").GetComponentInChildren<Text>();
+    }
+
+    void Update()
+    {
+        if (nomeInputText.text == "" || serieInputText.text == "")
+        {
+            continuarButton.interactable = false;
+        }
+        else
+        {
+            continuarButton.interactable = true;
+        }
+    }
+}
