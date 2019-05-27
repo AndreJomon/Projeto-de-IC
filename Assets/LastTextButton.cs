0ade8c2747452ac1ec2598f6ae97063bcd533eed
Andre <jomonzito2010@gmail.com>
2019-05-27 16:24:13 -0300

    Criadas scenes provisórias e botões de mudança de tela.

---
+++ Assets/LastTextButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LastTextButton : LoadScene
+{
+    private bool lastTextAppeared = false;
+
+    public void SetTrue()
+    {
+        lastTextAppeared = true;
+        ActiveObject();
+    }
+
+    private void ActiveObject()
+    {
+        gameObject.SetActive(lastTextAppeared);
+    }
+}
