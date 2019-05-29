30370a0346f4f76a41722ae8e06addf906e0ea12
Rafael Nicoletti <40860359+RafaelGNicoletti@users.noreply.github.com>
2019-05-29 11:19:51 -0300

    Scripts comentados parcialmente

--- Assets/Scripts/AreaProfessor/ProfButtonController.cs
+++ Assets/Scripts/AreaProfessor/ProfButtonController.cs
@@ -3,25 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Classe que controla os botões do painel do professor
+/// </summary>
 [CreateAssetMenu(fileName = "ProfButtonController", menuName = "My Assets/Professor Area Button Contrller")]
 public class ProfButtonController : ScriptableObject
 {
     private Player playerTemp;
     public static int selectedSlot = -1;
 
+    /// <summary>
+    /// Função que gera o resumo dos jogadores em um arquivo externo
+    /// </summary>
     public void ResumoDeJogadores()
     {
         DataManager.instance.PlayersSummary();
-
-        Debug.Log(playerTemp.ToString());
     }
 
+    /// <summary>
+    /// Função que armazena o slot selecionado e chama o painel de confirmação para deletar o player do slot
+    /// </summary>
+    /// <param name="buttonInfo"></param>
     public void RemovePlayer(SavegameLoadButton buttonInfo)
     {
         selectedSlot = buttonInfo.GetPlayer().GetSlot();
         GameObject.Find("AccessManager").GetComponent<ProfessorManager>().ConfirmPanel();
     }
 
+    /// <summary>
+    /// Função que remove o player selecionado e atualiza a tela
+    /// </summary>
     public void ConfirmDeletePlayer()
     {
         SaveManager.instance.DeletePlayer(selectedSlot);
@@ -29,6 +40,9 @@ public class ProfButtonController : ScriptableObject
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
+    /// <summary>
+    /// Cancela a remoção do jogador e remove o painel de confirmação
+    /// </summary>
     public void CancelDeletePlayer()
     {
         GameObject.Find("ConfirmPanel").SetActive(false);
--- Assets/Scripts/AreaProfessor/ProfessorManager.cs
+++ Assets/Scripts/AreaProfessor/ProfessorManager.cs
@@ -7,26 +7,44 @@ using UnityEngine;
 /// </summary>
 public class ProfessorManager : MonoBehaviour
 {
+    /// <summary>
+    /// Painéis que devem ser exibidos em momentos específicos
+    /// </summary>
+    /// Painel de verificação de login
     [SerializeField] private GameObject passwordPanel;
+    /// Painel da área principal do professor
     [SerializeField] private GameObject professorArea;
+    /// Painel de troca de senha
     [SerializeField] private GameObject changePassord;
+    /// Painel de exibição de mensagem de erro
     [SerializeField] private GameObject messagePanel;
+    /// Painel de confirmação de remoção de jogador
     [SerializeField] private GameObject confirmPanel;
 
+    /// <summary>
+    /// Campos de entrada de informação para a troca de senha
+    /// </summary>
     [SerializeField] private UnityEngine.UI.InputField curPassword;
     [SerializeField] private UnityEngine.UI.InputField newPassword;
     [SerializeField] private UnityEngine.UI.InputField newPasswordRewrite;
 
     [SerializeField] private UnityEngine.UI.Text messageText;
 
+    private readonly string senhaPadrao = "1234";
+
     private void Start()
     {
+        // Se não existe uma senha salva no PlayerPrefs, salva uma senha padrão
         if (!PlayerPrefs.HasKey("Senha"))
         {
-            PlayerPrefs.SetString("Senha", "1234");
+            PlayerPrefs.SetString("Senha", senhaPadrao);
         }
     }
 
+    /// <summary>
+    /// Função que verifica se a senha está correta para liberar o acesso ao professor
+    /// </summary>
+    /// <param name="password"></param>
     public void CheckPassword(UnityEngine.UI.InputField password)
     {
         if (PlayerPrefs.GetString("Senha") == password.text)
@@ -41,11 +59,17 @@ public class ProfessorManager : MonoBehaviour
         }
     }
     
+    /// <summary>
+    /// Função que ativa o painel de mudança de senha
+    /// </summary>
     public void ChangePasswordPanel()
     {
         changePassord.SetActive(true);
     }
 
+    /// <summary>
+    /// Função que realiza a mudança de senha
+    /// </summary>
     public void ChangePassword()
     {
         if (curPassword.text == PlayerPrefs.GetString("Senha"))
@@ -75,6 +99,9 @@ public class ProfessorManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Função que retorna ao painel do professor
+    /// </summary>
     public void GoToProfessorAccessPanel()
     {
         ClearPasswordFields();
@@ -83,6 +110,9 @@ public class ProfessorManager : MonoBehaviour
         changePassord.SetActive(false);
     }
 
+    /// <summary>
+    /// Função que limpa os campos da troca de senha
+    /// </summary>
     public void ClearPasswordFields()
     {
         curPassword.text = "";
@@ -90,6 +120,11 @@ public class ProfessorManager : MonoBehaviour
         newPasswordRewrite.text = "";
     }
 
+    /// <summary>
+    /// Função encarregada de mostrar uma mensagem na tela durante 5s
+    /// </summary>
+    /// <param name="message"></param>
+    /// <returns></returns>
     public IEnumerator ShowMessage(string message)
     {
         messagePanel.SetActive(true);
@@ -98,11 +133,17 @@ public class ProfessorManager : MonoBehaviour
         messagePanel.SetActive(false);
     }
 
+    /// <summary>
+    /// Função para resetar a senha para a senha padrão - utilizada para testes
+    /// </summary>
     public void ResetSenha()
     {
-        PlayerPrefs.SetString("Senha", "SenhaPadrao");
+        PlayerPrefs.SetString("Senha", senhaPadrao);
     }
 
+    /// <summary>
+    /// Função para ativar o painel de confirmação de remoção de jogadores
+    /// </summary>
     public void ConfirmPanel()
     {
         confirmPanel.SetActive(true);
