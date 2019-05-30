fc3174706eb847f1f79410842812d627765b9e4e
gustavo <gustavo.castilho@ufabc.edu.br>
2019-05-30 09:57:50 -0300

    mais Animações quiz

---
+++ Assets/Scripts/Quiz/InitialButtonsQuiz.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InitialButtonsQuiz : MonoBehaviour
+{
+    private Animator canvasAnimator;
+
+    void Start()
+    {
+        canvasAnimator = GameObject.Find("Canvas").GetComponent<Animator>();
+    }
+    
+    public void SetDifficultAndNextScreen()
+    {
+        //código de setar a dificuldade aqui
+        canvasAnimator.SetTrigger("difficult_out");
+    }
+
+    public void StartQuiz()
+    {
+        canvasAnimator.SetTrigger("comecar");
+    }
+}
