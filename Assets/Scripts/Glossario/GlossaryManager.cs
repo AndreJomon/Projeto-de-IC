42951450c11f163e364e043768dc35e0264c8060
Rafael Nicoletti <40860359+RafaelGNicoletti@users.noreply.github.com>
2019-06-06 11:57:08 -0300

    Glossário Update

--- Assets/Scripts/Glossario/GlossaryManager.cs
+++ Assets/Scripts/Glossario/GlossaryManager.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class GlossaryManager : MonoBehaviour
 {
+    public GlossaryManager instance;
     public GlossaryItems glossaryList;
 
     public GameObject glossaryPrefab;
     public GameObject glossaryWindow;
-    private List<GameObject> glossaryItems = new List<GameObject>();
+
+    public UnityEngine.UI.Text searchBarText;
+
+    [SerializeField] private List<GameObject> glossaryItems = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
         for (int i = 0; i < glossaryList.glossary.Count; i++)
         {
             GameObject temp = Instantiate(glossaryPrefab, glossaryWindow.transform);
@@ -21,11 +34,48 @@ public class GlossaryManager : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateGlossaryWindow(GlossaryItems glossaryListToShow)
     {
-        
+        glossaryItems.Clear();
+
+        for (int i = 0; i < glossaryListToShow.glossary.Count; i++)
+        {
+            GameObject temp = Instantiate(glossaryPrefab, glossaryWindow.transform);
+            temp.GetComponent<GlossaryPrefabSelfManager>().UpdatePrefabs(glossaryList.glossary[i]);
+            glossaryItems.Add(temp);
+        }
     }
 
-    
+    public GlossaryItems SortGlossaryItemList(GlossaryItems listToSort)
+    {
+        listToSort.glossary.Sort(delegate (GlossaryElement a, GlossaryElement b)
+        {
+            return a.expression.CompareTo(b.expression);
+        });
+
+        return listToSort;
+    }
+
+    public GlossaryItems SearchExpression(GlossaryItems listToSearch, string expressionToFind)
+    {
+        List<GlossaryElement> temp = new List<GlossaryElement>();
+        temp = listToSearch.glossary.FindAll(delegate (GlossaryElement a)
+        {
+            if (a.expression.ToUpper().Contains(expressionToFind.ToUpper())) return true;
+            else return false;
+        });
+        listToSearch.glossary = temp;
+        return listToSearch;
+    }
+
+    public void TestSort()
+    {
+        Debug.Log(searchBarText.text);
+        foreach(GlossaryElement g in SearchExpression(glossaryList, searchBarText.text).glossary)
+        {
+            Debug.Log(g.expression);
+        }
+
+        //UpdateGlossaryWindow(SearchExpression(glossaryList, searchBarText.text));
+    }
 }
