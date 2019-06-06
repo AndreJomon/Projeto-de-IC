c6a7be3dcd979911a4e10492ba7d37a3b85c4c9b
gustavo <gustavo.castilho@ufabc.edu.br>
2019-06-06 10:39:23 -0300

    Animations acesso professor, ajustes StartMenu.unity e MenuBar.prefab;

--- Assets/Scripts/Glossario/dotsmenuButton.cs
+++ Assets/Scripts/Glossario/dotsmenuButton.cs
@@ -2,18 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class dotsmenuButton : MonoBehaviour
 {
     private Animator animator;
     private Button home, voltar, glossario;
+    private ColorBlock disabledButtonColorBlock, voltarOriginalColorBlock;
 
     private void Awake()
     {
-        animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();        
         home = GameObject.Find("home").GetComponent<Button>();
         voltar = GameObject.Find("voltar").GetComponent<Button>();
         glossario = GameObject.Find("glossario").GetComponent<Button>();
+        disabledButtonColorBlock = voltar.colors;
+        voltarOriginalColorBlock = voltar.colors;
+        
     }
 
     public void OpenMenu()
@@ -23,8 +28,17 @@ public class dotsmenuButton : MonoBehaviour
 
     public void EnableSubMenuButtons()
     {
-        home.interactable = true;
-        voltar.interactable = true;
+        if (SceneManager.GetActiveScene().name == "StartMenu")
+        {
+            voltar.interactable = false;
+            disabledButtonColorBlock.disabledColor = new Color(0.5f,0.5f,0.5f,0.5f);
+            voltar.colors = disabledButtonColorBlock;
+        }
+        else
+        {
+            voltar.interactable = true;
+        }
+        home.interactable = true;        
         glossario.interactable = true;
     }
 
@@ -32,6 +46,7 @@ public class dotsmenuButton : MonoBehaviour
     {
         home.interactable = false;
         voltar.interactable = false;
-        glossario.interactable = false;
+        glossario.interactable = false;        
+        voltar.colors = voltarOriginalColorBlock;
     }
 }
---
+++ Assets/Scripts/MainMenu/LampButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LampButton : MonoBehaviour
+{
+    private Button lampButton;
+    
+    private void Awake()
+    {
+        lampButton = GetComponent<Button>();
+        lampButton.interactable = false;
+    }
+}
