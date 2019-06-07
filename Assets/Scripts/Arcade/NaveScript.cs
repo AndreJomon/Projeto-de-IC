8a678c0be48aa4d3bec0aec8c086e9d5a7861425
Andre <jomonzito2010@gmail.com>
2019-06-07 16:59:09 -0300

    Continuando a montar o arcade (minigame 1).

---
+++ Assets/Scripts/Arcade/NaveScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaveScript : MonoBehaviour
+{
+    public GameObject proton, eletron, neutron;
+    private Vector3 shootSpawnLocation;
+
+    private void Start()
+    {
+        shootSpawnLocation = GetComponentInChildren<Transform>().position;
+    }
+
+    private void Shoot(GameObject particule)
+    {
+        Instantiate(particule, gameObject.transform.position, Quaternion.identity, transform);
+    }
+
+    public void ShootProton()
+    {
+        Shoot(proton);
+    }
+
+    public void ShootEletron()
+    {
+        Shoot(eletron);
+    }
+
+    public void ShootNeutron()
+    {
+        Shoot(neutron);
+    }
+}
