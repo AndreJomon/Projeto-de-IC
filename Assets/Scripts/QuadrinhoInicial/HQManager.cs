72b0ca0f912135808ef974ecac684f5aaa43ab35
Rafael Nicoletti <40860359+RafaelGNicoletti@users.noreply.github.com>
2019-06-12 16:11:03 -0300

    Quadrinho e ajustes na área do professor

---
+++ Assets/Scripts/QuadrinhoInicial/HQManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HQManager : MonoBehaviour
+{
+    public Animator animator;
+
+    public void ToggleOff(GameObject item)
+    {
+        item.SetActive(false);
+    }
+
+    public void ToggleOn(GameObject item)
+    {
+        item.SetActive(true);
+    }
+
+    public void SetHQTrigger(string trigger)
+    {
+        animator.SetTrigger(trigger);
+    }
+
+    public void PlayVideo(GameObject videoContainer)
+    {
+        StartCoroutine(VideoManager.instance.PlayVideo(videoContainer.GetComponent<PlayVideoOnMouseOver>().video));
+    }
+}
