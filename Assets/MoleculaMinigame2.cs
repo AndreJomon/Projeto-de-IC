58ec37ce3a990a4c01269340d2c28da4b7cd9ade
Andre <jomonzito2010@gmail.com>
2019-06-19 13:15:46 -0300

    Minigame de balões sendo terminado. Imagens provisórias deletadas.

---
+++ Assets/MoleculaMinigame2.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoleculaMinigame2: MonoBehaviour
+{
+    public string name;
+}
---
+++ Assets/Scripts/Quintal/VisualStyleChoicer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualStyleChoicer : MonoBehaviour
+{
+    public GameObject imagemNeutra;
+    public GameObject imagemAtualizada;
+    public int flagAtiva;
+    private SaveManager saveManager;
+
+    private void Start()
+    {
+        saveManager = SaveManager.instance;
+        if (saveManager.player.GetBeatPartStatus(flagAtiva))
+        {
+            imagemAtualizada.SetActive(true);
+            imagemNeutra.SetActive(false);
+        }
+        else
+        {
+            imagemAtualizada.SetActive(false);
+            imagemNeutra.SetActive(true);
+        }
+    }
+}
