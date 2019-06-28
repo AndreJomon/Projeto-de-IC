19e16082df40b6006be2c61da7869722b1fca1e9
Andre <jomonzito2010@gmail.com>
2019-06-28 17:49:37 -0300

    Cena Final feita

---
+++ Assets/FinalScene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class FinalScene : MonoBehaviour
+{
+
+    public GameObject gameObjectToAppear;
+    public VideoClip video;
+    public RawImage rawImage;
+    public string nextScene;
+    private int clickCount = 0;
+
+    
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            if (clickCount == 0)
+            {
+                clickCount++;
+                gameObjectToAppear.SetActive(true);
+                StartCoroutine(VideoManager.instance.PlayVideo(video));
+            }
+            else
+            {
+                LoadScene.SceneLoader(nextScene);
+            }
+        }
+    }
+}
