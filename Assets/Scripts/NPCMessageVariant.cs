eae063ce29e9d68ef5ff10860be2c810bdfa2c10
Andre <jomonzito2010@gmail.com>
2019-07-01 16:21:56 -0300

    Animação da vela colocada

--- Assets/Scripts/NPCMessageVariant.cs
+++ Assets/Scripts/NPCMessageVariant.cs
@@ -6,6 +6,7 @@ public class NPCMessageVariant : NPCMessage
 {
     public DeafText alterMessage;
     public GameObject botaoBolo;
+    public GameObject velas;
 
     private void Start()
     {
@@ -14,6 +15,7 @@ public class NPCMessageVariant : NPCMessage
         if (tempPlayer.GetBeatPartStatus(0) && tempPlayer.GetBeatPartStatus(1) && tempPlayer.GetBeatPartStatus(2) && tempPlayer.GetBeatPartStatus(3))
         {
             balloonMessage = alterMessage;
+            velas.SetActive(true);
             botaoBolo.SetActive(true);
         }
     }
