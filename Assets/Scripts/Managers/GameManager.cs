bb5819337aca639997cdb66786a82f9e4c670d7d
Andre <jomonzito2010@gmail.com>
2019-07-19 12:43:33 -0300

    Correções

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -26,7 +26,7 @@ public class GameManager : MonoBehaviour {
                     correctAnswer[1] = true;
                 }
 
-                if (neutrons == nEletrons)
+                if (neutrons == nNeutrons)
                 {
                     correctAnswer[2] = true;
                 }
